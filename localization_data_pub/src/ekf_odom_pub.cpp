14b631bfcd063d78d9012a155749e2c29f478434
Changwhan Lee <zeta0707@gmail.com>
2022-12-03 14:29:53 +0900

    Correct Robot physical value

--- localization_data_pub/src/ekf_odom_pub.cpp
+++ localization_data_pub/src/ekf_odom_pub.cpp
@@ -46,11 +46,17 @@ const double initialTheta = 0.00000000001;
 const double PI = 3.141592;
 
 // Robot physical constants
-const double TICKS_PER_REVOLUTION = 620; // For reference purposes.
-const double WHEEL_RADIUS = 0.033; // Wheel radius in meters
-const double WHEEL_BASE = 0.17; // Center of left tire to center of right tire
-const double TICKS_PER_METER = 3100; // Original was 2800
- 
+//const double TICKS_PER_REVOLUTION = 620; // For reference purposes.
+//const double WHEEL_RADIUS = 0.033; // Wheel radius in meters
+//const double WHEEL_BASE = 0.17; // Center of left tire to center of right tire
+//const double TICKS_PER_METER = 3100; // Original was 2800
+
+// Robot physical constants
+const double TICKS_PER_REVOLUTION = 102.0;  // For reference purposes.
+const double WHEEL_RADIUS = 0.065;          // Wheel radius in meters
+const double WHEEL_BASE = 0.17;             // Center of left tire to center of right tire
+const double TICKS_PER_METER = 250.0;       // Original was 2800
+
 // Distance both wheels have traveled
 double distanceLeft = 0;
 double distanceRight = 0;
