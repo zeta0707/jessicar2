100e011c8a802df6b94bd38c3c002af787c32df6
zeta0707 <zeta0707@gmail.com>
2023-04-11 20:10:56 +0900

    Enable imu after fix unstable imu topic
    1. Change IMU loop, simply use dmpGetCurrentFIFOPacket
    2. Remove unused dmpGetEuler, dmpGetYawPitchRoll, dmpGetAccel and dmpGetAccel
    3. Enable imu_used

--- arduino/motorEncLedMpuRos32.ino
+++ arduino/motorEncLedMpuRos32.ino
@@ -33,11 +33,6 @@
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
 #include "Wire.h"
 #endif
-#define OUTPUT_READABLE_QUATERNION
-//#define OUTPUT_READABLE_EULER
-//#define OUTPUT_READABLE_YAWPITCHROLL
-//#define OUTPUT_READABLE_REALACCEL
-//#define OUTPUT_READABLE_WORLDACCEL
 #endif
 
 #define OLED 0
@@ -775,34 +770,13 @@ void mpu_loop() {
   if (!dmpReady) return;
 
   // wait for MPU interrupt or extra packet(s) available
-  if (!mpuInterrupt && fifoCount < packetSize) return;
+  if (!mpuInterrupt) return;
 
   // reset interrupt flag and get INT_STATUS byte
   mpuInterrupt = false;
   mpuIntStatus = mpu.getIntStatus();
-
-  // get current FIFO count
-  fifoCount = mpu.getFIFOCount();
-
-  // check for overflow (this should never happen unless our code is too inefficient)
-  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
-    // reset so we can continue cleanly
-    mpu.resetFIFO();
-    DEBUG_PRINTLN(F("FIFO overflow!"));
-
-    // otherwise, check for DMP data ready interrupt (this should happen frequently)
-  } else if (mpuIntStatus & 0x02) {
-    // wait for correct available data length, should be a VERY short wait
-    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
-    // read a packet from FIFO
-    mpu.getFIFOBytes(fifoBuffer, packetSize);
-
-    // track FIFO count here in case there is > 1 packet available
-    // (this lets us immediately read more without waiting for an interrupt)
-    fifoCount -= packetSize;
-
-#ifdef OUTPUT_READABLE_QUATERNION
+  
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) {  // Get the Latest packet
     // display quaternion values in easy matrix form: w x y z
     mpu.dmpGetQuaternion(&q, fifoBuffer);
     DEBUG_PRINT("quat\t");
@@ -819,61 +793,5 @@ void mpu_loop() {
     quat_msg.z = q.z;
 
     quat_pub.publish(&quat_msg);
-#endif
-
-#ifdef OUTPUT_READABLE_EULER
-    // display Euler angles in degrees
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    mpu.dmpGetEuler(euler, &q);
-    DEBUG_PRINT("euler\t");
-    DEBUG_PRINT(euler[0] * 180 / M_PI);
-    DEBUG_PRINT("\t");
-    DEBUG_PRINT(euler[1] * 180 / M_PI);
-    DEBUG_PRINT("\t");
-    DEBUG_PRINTLN(euler[2] * 180 / M_PI);
-#endif
-
-#ifdef OUTPUT_READABLE_YAWPITCHROLL
-    // display Euler angles in degrees
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-    DEBUG_PRINT("ypr\t");
-    DEBUG_PRINT(ypr[0] * 180 / M_PI);
-    DEBUG_PRINT("\t");
-    DEBUG_PRINT(ypr[1] * 180 / M_PI);
-    DEBUG_PRINT("\t");
-    DEBUG_PRINTLN(ypr[2] * 180 / M_PI);
-#endif
-
-#ifdef OUTPUT_READABLE_REALACCEL
-    // display real acceleration, adjusted to remove gravity
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    mpu.dmpGetAccel(&aa, fifoBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-    DEBUG_PRINT("areal\t");
-    DEBUG_PRINT(aaReal.x);
-    DEBUG_PRINT("\t");
-    DEBUG_PRINT(aaReal.y);
-    DEBUG_PRINT("\t");
-    DEBUG_PRINTLN(aaReal.z);
-#endif
-
-#ifdef OUTPUT_READABLE_WORLDACCEL
-    // display initial world-frame acceleration, adjusted to remove gravity
-    // and rotated based on known orientation from quaternion
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    mpu.dmpGetAccel(&aa, fifoBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-    mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
-    DEBUG_PRINT("aworld\t");
-    DEBUG_PRINT(aaWorld.x);
-    DEBUG_PRINT("\t");
-    DEBUG_PRINT(aaWorld.y);
-    DEBUG_PRINT("\t");
-    DEBUG_PRINTLN(aaWorld.z);
-#endif
   }
 }
